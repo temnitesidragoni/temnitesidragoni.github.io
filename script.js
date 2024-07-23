f0269650cc1708d479a6ecfce8796c0f3d0ce838
Renat Babacaev <renat.babacaev@gmail.com>
2024-07-23 13:56:04 +0300

    Correct the variable names and id names in the script

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const navbar = document.getElementById("navbar");
 
 const main = document.getElementById("main");
-const maincontent = document.getElementById("maincontent");
+
+const section_home = document.getElementById("section-home");
+const section_news = document.getElementById("section-news");
+const section_crew = document.getElementById("section-crew");
+const section_shop = document.getElementById("section-shop");
+
 const footer = document.getElementById("footer");
 
 
